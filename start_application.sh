d5af3fdb26e16951cb1e314fe77ef69cd63eeb13
Jonatan Landsberg <jonatan.landsberg@omegapoint.se>
2017-10-29 14:36:45 +0100

    specify profile before jar file

--- start_application.sh
+++ start_application.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 touch /var/log/cloud-reverser.log
-nohup java -jar /tmp/*.jar -Dspring.profiles.active=production > /var/log/cloud-reverser.log 2>&1 &
+nohup java -jar -Dspring.profiles.active=production /tmp/*.jar > /var/log/cloud-reverser.log 2>&1 &
